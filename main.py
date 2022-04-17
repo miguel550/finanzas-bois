dadab9df526175249c11d06282cd7ff855bffc1e
Miguel <miguel.almonte1@gmail.com>
2022-04-17 01:10:15 -0400

    fix unbalanced transactions, add better args parser and add relative date search

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ import decimal
 import sys
 import io
 import pathlib
+import argparse
+import time
 
+import parsedatetime
 from beancount.ingest import importer, extract
 from beancount.query.query import run_query
 from beancount.loader import load_string
@@ -48,6 +51,7 @@ class Importer(importer.ImporterProtocol):
 
     def get_postings_from_payment(self, payment):
         postings = []
+        # TODO: Verify who is a boi
         postings.append(data.Posting(
             f'Liabilities:Bois:{payment["who"]}',
             amount.Amount(decimal.Decimal(payment['amount']), 'DOP'),
@@ -69,6 +73,7 @@ class Importer(importer.ImporterProtocol):
     def create_debt_transaction_from_expense(self, file, expense, bois):
         postings = []
         amnt = decimal.Decimal(expense['amount'])
+        # TODO: Verify who is a boi
         postings.append(data.Posting(
             f'Liabilities:Bois:{expense["who"]}',
             amount.Amount(amnt, 'DOP'),
@@ -77,7 +82,8 @@ class Importer(importer.ImporterProtocol):
             None,
             None
         ))
-        all_bois = expense['split_between']
+        # TODO: Verify all_bois are bois
+        all_bois = set(expense['split_between'] + [expense['who']])
         debt = round(amnt/len(all_bois), 2)
         for boi in all_bois:
             if boi == expense['who']:
@@ -92,7 +98,7 @@ class Importer(importer.ImporterProtocol):
             ))
         postings.append(data.Posting(
             f'Assets:CoroPago',
-            -amount.Amount(amnt - debt*(len(bois)-1), 'DOP'),
+            -amount.Amount(amnt - debt*(len(all_bois)-1), 'DOP'),
             None,
             None,
             None,
@@ -117,6 +123,7 @@ class Importer(importer.ImporterProtocol):
         for boi in finanzas_dict['bois']:
             entries.append(data.Open(
                 self.get_new_meta(file.name),
+                # TODO: Add date from first time the boi is shown in expenses
                 # here I'm assuming first datetime is the earliest date
                 datetime.date.fromisoformat(finanzas_dict['expenses'][0]['when']),
                 f'Liabilities:Bois:{boi}',
@@ -133,6 +140,7 @@ class Importer(importer.ImporterProtocol):
             data.Booking.NONE
         ))
         for expense in finanzas_dict['expenses']:
+            # TODO: Verify who is a boi
             entries.append(data.Transaction(
                 self.get_new_meta(file.name),
                 datetime.date.fromisoformat(expense['when']),
@@ -152,6 +160,7 @@ class Importer(importer.ImporterProtocol):
             )
 
         for payment in finanzas_dict['payments']:
+            # TODO: Verify to is a boi
             entries.append(data.Transaction(
                 self.get_new_meta(file.name),
                 datetime.date.fromisoformat(payment['when']),
@@ -181,15 +190,44 @@ def loads():
     return file.read()
 
 
+def parse_datetime(relative_or_iso: str) -> str:
+    cal = parsedatetime.Calendar()
+    time_struct, parse_status = cal.parse(relative_or_iso)
+    return f'{time_struct[0]}-{time_struct[1]}-{time_struct[2]}'
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Lo paga malo.')
+
+    # TODO: Add choices to be only bois names
+    parser.add_argument('boi_name', help='Nombre del boi al que quieres ver quienes les deben.', type=str)
+    parser.add_argument('-d', '--date', help='Relative or ISO date', type=str)
+    parser.add_argument('--keepdb', help='', action=argparse.BooleanOptionalAction)
+
+    args = parser.parse_args()
+    return args
+
+
 def main():
     if len(sys.argv) == 1:
         return
-    a_quien_se_le_debe = sys.argv[1]
-    entries, errors, options = load_string(loads())
+    args = get_args()
+    a_quien_se_le_debe = args.boi_name
+    # TODO: Check if the output doesnt have unbalanced transactions uwu
+    file_content = loads()
+    if args.keepdb:
+        with open('db.beancount', 'w') as f:
+            f.write(file_content)
+    entries, errors, options = load_string(file_content)
+    if not a_quien_se_le_debe:
+        return
+    select = f"select account, sum(position) where '{a_quien_se_le_debe}' in tags"
+    if args.date:
+        select += f" and date = DATE('{parse_datetime(args.date)}')"
     res_types, res_rows = run_query(
         entries,
         options,
-        f"select account, sum(position) where '{a_quien_se_le_debe}' in tags",
+        select,
         numberify=True
     )
     for row in res_rows:
