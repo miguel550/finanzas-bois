3a646bcf23696ffd0468162280967e02166abd33
Miguel <miguel.almonte1@gmail.com>
2022-04-24 18:05:40 -0400

    update program

--- main.py
+++ main.py
@@ -52,11 +52,20 @@ class BoizRegisterImporter(importer.ImporterProtocol):
         ))
         return postings
 
+    def get_boi_account(self, boi_name):
+        if not self.bois:
+            raise Exception('Bois var is not defined')
+        if boi_name not in self.bois:
+            raise Exception(f'Boi {boi_name} doesnt exists')
+        return f'Liabilities:Bois:{boi_name}'
+
+    def get_coro_pago_account(self):
+        return 'Assets:CoroPago'
+
     def get_postings_from_payment(self, payment):
         postings = []
-        # TODO: Verify who is a boi
         postings.append(data.Posting(
-            f'Liabilities:Bois:{payment["who"]}',
+            self.get_boi_account(payment["who"]),
             amount.Amount(decimal.Decimal(payment['amount']), 'DOP'),
             None,
             None,
@@ -64,7 +73,7 @@ class BoizRegisterImporter(importer.ImporterProtocol):
             None
         ))
         postings.append(data.Posting(
-            'Assets:CoroPago',
+            self.get_coro_pago_account(),
             -amount.Amount(decimal.Decimal(payment['amount']), 'DOP'),
             None,
             None,
@@ -76,23 +85,21 @@ class BoizRegisterImporter(importer.ImporterProtocol):
     def create_debt_transaction_from_expense(self, file, expense, bois):
         postings = []
         amnt = decimal.Decimal(expense['amount'])
-        # TODO: Verify who is a boi
         postings.append(data.Posting(
-            f'Liabilities:Bois:{expense["who"]}',
+            self.get_boi_account(expense["who"]),
             amount.Amount(amnt, 'DOP'),
             None,
             None,
             None,
             None
         ))
-        # TODO: Verify all_bois are bois
         all_bois = set(expense['split_between'] + [expense['who']])
         debt = round(amnt/len(all_bois), 2)
         for boi in all_bois:
             if boi == expense['who']:
                 continue
             postings.append(data.Posting(
-                f'Liabilities:Bois:{boi}',
+                self.get_boi_account(boi),
                 -amount.Amount(debt, 'DOP'),
                 None,
                 None,
@@ -100,7 +107,7 @@ class BoizRegisterImporter(importer.ImporterProtocol):
                 None
             ))
         postings.append(data.Posting(
-            f'Assets:CoroPago',
+            self.get_coro_pago_account(),
             -amount.Amount(amnt - debt*(len(all_bois)-1), 'DOP'),
             None,
             None,
@@ -123,13 +130,14 @@ class BoizRegisterImporter(importer.ImporterProtocol):
         with open(file.name) as f:
             finanzas_dict = json.loads(f.read())
         entries = []
+        self.bois = finanzas_dict['bois']
         for boi in finanzas_dict['bois']:
             entries.append(data.Open(
                 self.get_new_meta(file.name),
                 # TODO: Add date from first time the boi is shown in expenses
                 # here I'm assuming first datetime is the earliest date
                 datetime.date.fromisoformat(finanzas_dict['expenses'][0]['when']),
-                f'Liabilities:Bois:{boi}',
+                self.get_boi_account(boi),
                 '',
                 data.Booking.NONE
             ))
@@ -138,12 +146,11 @@ class BoizRegisterImporter(importer.ImporterProtocol):
             self.get_new_meta(file.name),
             # here I'm assuming first datetime is the earliest date
             datetime.date.fromisoformat(finanzas_dict['expenses'][0]['when']),
-            'Assets:CoroPago',
+            self.get_coro_pago_account(),
             '',
             data.Booking.NONE
         ))
         for expense in finanzas_dict['expenses']:
-            # TODO: Verify who is a boi
             entries.append(data.Transaction(
                 self.get_new_meta(file.name),
                 datetime.date.fromisoformat(expense['when']),
@@ -163,7 +170,6 @@ class BoizRegisterImporter(importer.ImporterProtocol):
             )
 
         for payment in finanzas_dict['payments']:
-            # TODO: Verify to is a boi
             entries.append(data.Transaction(
                 self.get_new_meta(file.name),
                 datetime.date.fromisoformat(payment['when']),
@@ -182,11 +188,14 @@ CONFIG = [
 ]
 
 
+REGISTRO_PATH = str(pathlib.Path('./registro.json').absolute())
+
+
 def loads():
     file = io.StringIO()
     extract.extract(
         importer_config=CONFIG,
-        files_or_directories=[str(pathlib.Path('./registro.json').absolute())],
+        files_or_directories=[REGISTRO_PATH],
         output=file
     )
     file.seek(0)
@@ -199,11 +208,10 @@ def parse_datetime(relative_or_iso: str) -> str:
     return f'{time_struct[0]}-{time_struct[1]}-{time_struct[2]}'
 
 
-def get_args():
+def get_args(bois: list):
     parser = argparse.ArgumentParser(description='Lo paga malo.')
 
-    # TODO: Add choices to be only bois names
-    parser.add_argument('boi_name', help='Nombre del boi al que quieres ver quienes les deben.', type=str)
+    parser.add_argument('boi_name', choices=bois, help='Nombre del boi al que quieres ver quienes les deben.', type=str)
     parser.add_argument('-d', '--date', help='Relative or ISO date', type=str)
     parser.add_argument('--keepdb', help='', action=argparse.BooleanOptionalAction)
 
@@ -211,12 +219,18 @@ def get_args():
     return args
 
 
+def get_bois_names():
+    with open(REGISTRO_PATH, 'r') as f:
+        j = json.load(f)
+        return j['bois']
+
+
 # TODO: It should say why a boi owe that amount to the boi
 # Possible fix: modify narration to show for example 1/6th of Expense name
 def main():
     if len(sys.argv) == 1:
         return
-    args = get_args()
+    args = get_args(get_bois_names())
     a_quien_se_le_debe = args.boi_name
     # TODO: Check if the output doesnt have unbalanced transactions uwu
     file_content = loads()
